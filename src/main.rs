86ca243620ce89069e627c47fc7cf6eed8173507
Vy Tien <me@tienminhvy.com>
2025-10-17 09:02:14 +0700

    Update code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
+
+    let inmutable: i32 = 5;
+    println!("inmutable variable value: {}", inmutable);
+
+    // Cannot re-assign due to all variable are inmutable by default
+    // inmutable = 7;
+
+    let mut mutable: i32 = 8; // Need to mutate a variable? add 'mut' directive for it.
+    println!("Before mutation: {}", mutable);
+
+    mutable = 11;
+    println!("After mutation: {}", mutable);
 }
