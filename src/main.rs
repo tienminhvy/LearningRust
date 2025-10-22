3e44514ab86828bb782390839f2a10fc6396b680
Vy Tien <me@tienminhvy.com>
2025-10-22 11:52:31 +0700

    Add calling functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
+    another_function();
+    another_function_with_param(55555);
+}
+
+fn another_function() {
+    println!("!dlrow, olleH");
+}
+
+fn another_function_with_param(x: i32) {
+    println!("The value of x is {x}");
 }
