a5943c2bc9d4124e49f9e669cfad63723f3124c9
Vy Tien <me@tienminhvy.com>
2025-10-22 15:06:15 +0700

    Statement and expression compilation with functions

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,24 @@ fn main() {
     println!("Hello, world!");
     another_function();
     another_function_with_param(55555);
+    another_function_with_params(5, "pounds");
+
+    // statement and expression
+    
+    // Cannot do this
+    // let x = (let y = 5);
+
+    let x: i32 = {
+        let y: i32 = 5;
+        y + 1
+    };
+    println!("{x} is a number");
+
+    let value: &str = function_with_returning_value();
+    println!("{value}");
+
+    let x = plus_one(5);
+    println!("Value of x is {x}");
 }
 
 fn another_function() {
@@ -10,4 +28,19 @@ fn another_function() {
 
 fn another_function_with_param(x: i32) {
     println!("The value of x is {x}");
+}
+
+fn another_function_with_params(x: i32, label: &str) {
+    println!("The value of x is {x} {label}");
+}
+
+fn function_with_returning_value() -> &'static str {
+    return "Lorem ipsum dolor sit amet...";
+}
+
+fn plus_one(x: i32) -> i32 {
+    // this works
+    x + 1
+    // but this is not
+    // x + 1;
 }
