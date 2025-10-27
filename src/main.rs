ccc0846d8c61f660711a8ccd47f021898d434cc3
Vy Tien <me@tienminhvy.com>
2025-10-27 09:01:10 +0700

    add destructuring tuple code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     println!("Hello, world!");
-    matching()
+    matching();
+    destructuring_tuple();
 }
 
 fn matching() {
@@ -26,4 +27,20 @@ fn matching() {
     };
 
     println!("{} -> {}", boolean, binary);
+}
+
+fn destructuring_tuple() {
+    let triple = (1, 1, -2);
+    println!("Tell me about {:?}", triple);
+    // Match can be used to destructure a tuple
+    match triple {
+        // Destructure the second and third elements
+        (0, y, z) => println!("First is `0`, `y` is {:?}, and `z` is {:?}", y, z),
+        (1, ..)  => println!("First is `1` and the rest doesn't matter"),
+        (.., 2)  => println!("last is `2` and the rest doesn't matter"),
+        (3, .., 4)  => println!("First is `3`, last is `4`, and the rest doesn't matter"),
+        // `..` can be used to ignore the rest of the tuple
+        _      => println!("It doesn't matter what they are"),
+        // `_` means don't bind the value to a variable
+    }
 }
