f0a3a2fccef58be30ec74cacc36826a6038d0d18
Vy Tien <me@tienminhvy.com>
2025-10-27 12:38:33 +0700

    add destructing enum code

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ fn main() {
     matching();
     destructuring_tuple();
     destructuring_array();
+    destructuring_enum();
 }
 
 fn matching() {
@@ -82,5 +83,41 @@ fn destructuring_array() {
             "array[0] = {}, middle = {:?}, array[2] = {}",
             first, middle, last
         ),
+    }
+}
+
+fn destructuring_enum() {
+    #[allow(dead_code)]
+    enum Color {
+        Red,
+        Green,
+        Blue,
+        // These likewise tie `u32` tuples to different names: color models.
+        RGB(u32, u32, u32),
+        HSV(u32, u32, u32),
+        HSL(u32, u32, u32),
+        CMY(u32, u32, u32),
+        CMYK(u32, u32, u32, u32)
+    }
+
+    let color: Color = Color::RGB(122, 77, 40);
+
+    // An `enum` can be destructured using a `match`.
+    match color {
+        Color::Red   => println!("The color is Red!"),
+        Color::Blue  => println!("The color is Blue!"),
+        Color::Green => println!("The color is Green!"),
+        Color::RGB(r, g, b) =>
+            println!("Red: {}, green: {}, and blue: {}!", r, g, b),
+        Color::HSV(h, s, v) =>
+            println!("Hue: {}, saturation: {}, value: {}!", h, s, v),
+        Color::HSL(h, s, l) =>
+            println!("Hue: {}, saturation: {}, lightness: {}!", h, s, l),
+        Color::CMY(c, m, y) =>
+            println!("Cyan: {}, magenta: {}, yellow: {}!", c, m, y),
+        Color::CMYK(c, m, y, k) =>
+            println!("Cyan: {}, magenta: {}, yellow: {}, key (black): {}!",
+                c, m, y, k),
+        // Don't need another arm because all variants have been examined
     }
 }
