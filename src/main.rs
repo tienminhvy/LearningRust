74aeeddce4b91641fdc9f227f55e7f0f534465dc
Vy Tien <me@tienminhvy.com>
2025-10-27 09:14:28 +0700

    add destructing array with binding sigil

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     println!("Hello, world!");
     matching();
     destructuring_tuple();
+    destructuring_array();
 }
 
 fn matching() {
@@ -42,5 +43,44 @@ fn destructuring_tuple() {
         // `..` can be used to ignore the rest of the tuple
         _      => println!("It doesn't matter what they are"),
         // `_` means don't bind the value to a variable
+    }
+}
+
+fn destructuring_array() {
+    // Try changing the values in the array, or make it a slice!
+    let array: [i32; 3] = [1, -2, 6];
+
+    match array {
+        // Binds the second and the third elements to the respective variables
+        [0, second, third] =>
+            println!("array[0] = 0, array[1] = {}, array[2] = {}", second, third),
+
+        // Single values can be ignored with _
+        [1, _, third] => println!(
+            "array[0] = 1, array[2] = {} and array[1] was ignored",
+            third
+        ),
+
+        // You can also bind some and ignore the rest
+        [-1, second, ..] => println!(
+            "array[0] = -1, array[1] = {} and all the other ones were ignored",
+            second
+        ),
+        // The code below would not compile
+        // [-1, second] => ...
+
+        // Or store them in another array/slice (the type depends on
+        // that of the value that is being matched against)
+        [3, second, tail @ ..] => println!(
+            "array[0] = 3, array[1] = {} and the other elements were {:?}",
+            second, tail
+        ),
+
+        // Combining these patterns, we can, for example, bind the first and
+        // last values, and store the rest of them in a single array
+        [first, middle @ .., last] => println!(
+            "array[0] = {}, middle = {:?}, array[2] = {}",
+            first, middle, last
+        ),
     }
 }
