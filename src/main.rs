56fa0458309efa1f92d01e330112fe9f072660e5
Vy Tien <me@tienminhvy.com>
2025-10-27 08:56:17 +0700

    Matching structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
     println!("Hello, world!");
+    matching()
+}
+
+fn matching() {
+    let number: i32 = 99;
+
+    println!("Current number is {}", number);
+
+    match number {
+        1 => {
+            println!("It's one!");
+        },
+        3 | 5 | 7 | 9 | 11 => println!("A prime value"),
+        13..19 => println!("In range 13..19, not including 19"),
+        23..=29 => println!("In range 23..29"),
+        _ => println!("The default case") // cannot be missed
+    };
+
+    let boolean: bool = false;
+
+    let binary: i32 = match boolean {
+        true => 1,
+        false => 0
+    };
+
+    println!("{} -> {}", boolean, binary);
 }
